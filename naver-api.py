fe8b5a872fedc85e1f01fe62653b3aac6a7a3102
Sang <rsw7876@naver.com>
2023-05-04 10:25:20 +0900

    naver shopping api test

---
+++ naver-api.py
@@ -0,0 +1,12 @@
+import requests
+
+URL = 'https://openapi.naver.com/v1/search/shop.json?query=GTX3060'
+headers = {    
+    'X-Naver-Client-Id' : 'SSO3GRfmtsics4OrPq92',
+    'X-Naver-Client-Secret' : 'XKC8TVhwpT'
+    }
+
+
+res = requests.get(URL, headers=headers)
+
+print(res.text)
