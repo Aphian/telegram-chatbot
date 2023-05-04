3f4e84df5a636716789df224a9b677fc851e599f
Sang <rsw7876@naver.com>
2023-05-04 15:37:41 +0900

    set Webhook & Flask Server

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request
+import requests
+import utils
+
+app = Flask('telegram-chatbot')
+
+TOKEN = '6178945223:AAFZMM3tqF_EDQvUiJc1Rvu2GhTbUz4YMwY'
+BASE_URL = f'https://api.telegram.org/bot{TOKEN}'
+
+@app.route('/telegram', methods=['POST']) 
+def telegram():
+    data = request.json
+    print('TELEGRAM에서 요청이 들어왔다!')
+    chat_id = data['message']['from']['id']
+    message = data['message']['text']
+    # print(chat_id, message)
+
+    # Response
+    if message == '주식':
+        return_msg = utils.get_kospi()
+
+    elif message in ['로또', 'lotto', 'Lotto']:
+        return_msg = utils.get_lotto()
+
+    elif message.split()[0] == '쇼핑' :
+        item = message.split()[1]
+        return_msg = utils.get_naver_shopping(item)
+
+    else:
+        return_msg = '모르는 명령어 입니다.'
+    
+    requests.get(BASE_URL + f'/sendMessage?chat_id={chat_id}&text={return_msg}')
+
+    return 'Telegram CHATBOT'
+
+# ~~~ '/hello'이렇게 요청이 들어오면, 아래 함수를 실행해라.
+@app.route('/hello')
+def hello():
+    # 'Hello World!!!' 라도 응답 해라.
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(port=80, debug=True)
